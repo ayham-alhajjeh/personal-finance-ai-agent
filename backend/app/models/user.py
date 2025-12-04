6b6fe8aa5771ed45cd9006beb0560ba36ecbbf9f
ayham-alhajjeh <ayhamalhajjeh05@gmail.com>
2025-12-04 00:56:54 -0600

    updated user model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -11,5 +11,7 @@ class UserDB(Base):
     id = Column(Integer, primary_key = True, index = True)
     email = Column(String, unique = True, index = True, nullable = False)
     name = Column(String, nullable = True)
+    created_at = Column(DateTime(timezone=True), server_default = func.now())
 
+    
     transactions = relationship("TransactionDB", back_populates = "user")
